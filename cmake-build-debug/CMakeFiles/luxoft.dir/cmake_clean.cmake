0f49acd0b5b6bbc5b52153597f8b1be58e9692cf
Alexander Sukhikh <xbox24_772@mail.ru>
2019-10-16 23:03:13 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/luxoft.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/luxoft.dir/main.cpp.o"
+  "luxoft.pdb"
+  "luxoft"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/luxoft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+
+int main(int cnt, char **args) {
+    return 0;
+}
