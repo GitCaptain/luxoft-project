87cd46b9f573ed603f609e921a3ca574c4753a5f
Alexander Sukhikh <xbox24_772@mail.ru>
2019-10-18 14:20:25 +0300

    File* - > streams

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-#include <functional>
 #include <string>
 #include <thread>
-#include <string.h>
 #include <fstream>
 #include <sys/mman.h>
 #include <memory>
 #include <iostream>
 #include <vector>
-#include <sstream>
 
 class FileException: public std::exception{
 
@@ -15,32 +12,30 @@ class FileException: public std::exception{
 public:
     FileException(const std::string& err_mes): err_mes(err_mes){}
 
-    std::string what(){
-        return err_mes;
+    const char* what() const noexcept override{
+        return err_mes.c_str();
     }
 
 };
 
+
+std::vector<std::exception_ptr> thread_exceptions;
 int thread_to_write = 0;
-void get_hash_and_write_thread(const std::string &str, int thread_num, std::shared_ptr<std::FILE> output_file){
+void get_hash_and_write_thread(const std::string &str, int thread_num, std::ofstream& output_file){
     try {
         std::hash<std::string> get_hash;
 
         auto hash = get_hash(str);
 
-        std::stringstream converter;
-        converter << hash;
-        std::string string_hash;
-        converter >> string_hash;
-
         while (thread_num != thread_to_write)
             std::this_thread::yield();
 
-        std::fwrite(string_hash.c_str(), string_hash.length(), 1, output_file.get());
+        output_file << hash;
         thread_to_write++;
     }
     catch(...){
-
+        if(thread_to_write == thread_num)
+            thread_to_write++;
     }
 }
 
@@ -58,16 +53,13 @@ int main(int argc, char **argv){
         if(argc < 3)
             throw std::invalid_argument("Paths to input file and output file should be given");
 
-        auto tmp_input_file_ptr = std::fopen(argv[1], "r");
-        if(tmp_input_file_ptr == nullptr)
+        std::ifstream input_file(argv[1], std::ios::in | std::ios::binary);
+        if(!input_file.is_open())
             throw FileException("Can't access input file");
-        std::unique_ptr<std::FILE, decltype(&close_file)> input_file(tmp_input_file_ptr, &close_file);
 
-
-        auto tmp_output_file_ptr = std::fopen(argv[2], "w");
-        if(tmp_output_file_ptr == nullptr)
+        std::ofstream output_file(argv[2]);
+        if(!output_file.is_open())
             throw FileException("Can't access output file");
-        std::shared_ptr<std::FILE> output_file(tmp_output_file_ptr, &close_file);
 
         unsigned long block_size_in_bytes = BYTES_PER_MEGABYTE;
 
@@ -77,16 +69,13 @@ int main(int argc, char **argv){
                 throw std::invalid_argument("Third argument must be positive integer value (block size in bytes)");
         }
 
-        int size_read;
         int thread_id = 0;
         do{
-            auto buf = std::unique_ptr<char*>(new char*[block_size_in_bytes]);
-            size_read = std::fread(buf.get(), block_size_in_bytes, 1, input_file.get());
-            std::thread(get_hash_and_write_thread, std::string(*buf), thread_id++, output_file);
-        }while(size_read == block_size_in_bytes);
+            std::unique_ptr<char[]> buf(new char[block_size_in_bytes]);
+            input_file.read(buf.get(), block_size_in_bytes);
+            std::thread(get_hash_and_write_thread, std::string(buf.get()), thread_id++, output_file);
+        }while(!input_file.eof());
 
-        if(!std::feof(input_file.get()))
-            throw FileException("Some error occurred while reading file");
 
         while(thread_to_write < thread_id)
             std::this_thread::yield();
