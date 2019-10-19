4459c665cfb9668f93db7f9db612a0a3d98db496
Alexander Sukhikh <xbox24_772@mail.ru>
2019-10-19 12:39:11 +0300

    keep intermadiate state

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <string>
 #include <thread>
 #include <fstream>
@@ -5,6 +6,78 @@
 #include <memory>
 #include <iostream>
 #include <vector>
+#include <mutex>
+using namespace std;
+
+class FileException: public std::exception{
+
+    std::string err_mes;
+public:
+    FileException(const std::string& err_mes): err_mes(err_mes){}
+
+    const char* what() const noexcept override{
+        return err_mes.c_str();
+    }
+
+};
+
+std::vector<std::exception_ptr> threads_exceptions;
+int thread_to_write = 0;
+std::mutex thread_mutex;
+
+void get_hash_and_write_thread(const std::shared_ptr<char[]>  &str, int thread_num, std::ofstream& output_file){
+    try {
+        std::hash<std::string> get_hash;
+        auto hash = get_hash(std::string(str.get()));
+
+        while (thread_num < thread_to_write)
+            std::this_thread::yield();
+
+        output_file << hash;
+        thread_to_write++;
+    }
+    catch(...){
+
+        thread_mutex.lock();
+        threads_exceptions.emplace_back(std::current_exception());
+        thread_mutex.unlock();
+
+        while (thread_num < thread_to_write)
+            std::this_thread::yield();
+        if(thread_num == thread_to_write)
+            thread_to_write++;
+    }
+}
+
+int main(){
+    std::ifstream input_file("in", std::ios::in | std::ios::binary);
+    if(!input_file.is_open())
+        throw FileException("Can't access input file");
+
+    std::ofstream output_file("out");
+    if(!output_file.is_open())
+        throw FileException("Can't access output file");
+
+    thread *t;
+    while(!input_file.eof()){
+        shared_ptr<char[]> buf(new char[1024]);
+        input_file.read(buf.get(), 1024-1);
+        get_hash_and_write_thread(std::ref(buf), 0, std::ref(output_file));
+        t = new thread(get_hash_and_write_thread, std::ref(buf), 0, std::ref(output_file));
+    }
+    t->join();
+}
+*/
+
+#include <string>
+#include <thread>
+#include <fstream>
+#include <sys/mman.h>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <mutex>
+
 
 class FileException: public std::exception{
 
@@ -19,31 +92,34 @@ public:
 };
 
 
-std::vector<std::exception_ptr> thread_exceptions;
+std::vector<std::exception_ptr> threads_exceptions;
 int thread_to_write = 0;
-void get_hash_and_write_thread(const std::string &str, int thread_num, std::ofstream& output_file){
+std::mutex thread_mutex;
+
+void get_hash_and_write_thread(const std::shared_ptr<char[]>  &str, int thread_num, std::ofstream& output_file){
     try {
         std::hash<std::string> get_hash;
 
-        auto hash = get_hash(str);
+        auto hash = get_hash(std::string(str.get()));
 
-        while (thread_num != thread_to_write)
+        while (thread_num < thread_to_write)
             std::this_thread::yield();
 
         output_file << hash;
         thread_to_write++;
     }
     catch(...){
-        if(thread_to_write == thread_num)
+
+        thread_mutex.lock();
+        threads_exceptions.emplace_back(std::current_exception());
+        thread_mutex.unlock();
+
+        while (thread_num < thread_to_write)
+            std::this_thread::yield();
+        if(thread_num == thread_to_write)
             thread_to_write++;
     }
 }
-
-void close_file(std::FILE* fp){
-    std::fclose(fp);
-}
-
-std::vector<std::exception_ptr> threads_exceptions;
 
 int main(int argc, char **argv){
 
@@ -70,11 +146,13 @@ int main(int argc, char **argv){
         }
 
         int thread_id = 0;
-        do{
-            std::unique_ptr<char[]> buf(new char[block_size_in_bytes]);
-            input_file.read(buf.get(), block_size_in_bytes);
-            std::thread(get_hash_and_write_thread, std::string(buf.get()), thread_id++, output_file);
-        }while(!input_file.eof());
+        std::thread t;
+        while(!input_file.eof()){
+            std::shared_ptr<char[]> buf(new char[block_size_in_bytes]);
+            input_file.read(buf.get(), block_size_in_bytes-1);
+            get_hash_and_write_thread(std::ref(buf), thread_id++, std::ref(output_file));
+            t = std::thread(get_hash_and_write_thread, std::ref(buf), thread_id++, std::ref(output_file));
+        }
 
 
         while(thread_to_write < thread_id)
@@ -91,6 +169,5 @@ int main(int argc, char **argv){
     catch(...){
         std::cout << "Something went wrong.\n";
     }
-
     return 0;
 }
